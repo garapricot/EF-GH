1abd7efe2cfd7abbef935348ecb70539f521eb85
garapricot <yerevanrockcity@gmail.com>
2016-08-27 18:06:26 +0400

    EF GH-

---
+++ EF GH/EF GH/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EF_GH
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
